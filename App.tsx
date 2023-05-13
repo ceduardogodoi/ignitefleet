0469d0ba5643adccaa84fa88e609810c5abc45de
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-05-13 09:35:02 -0300

    Fontes personalizadas + componente de Loading

--- App.tsx
+++ App.tsx
@@ -1,10 +1,32 @@
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
 
 import theme from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+
+        <Loading />
+      </>
+    );
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <SignIn />
