55b968dc77680cb1638711f3eff78cd68ac354cc
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-05-22 08:56:35 -0300

    Método generate

--- src/libs/realm/schemas/Historic.ts
+++ src/libs/realm/schemas/Historic.ts
@@ -1,5 +1,11 @@
 import { Realm } from '@realm/react';
 
+type GenerateProps = {
+  user_id: string;
+  license_plate: string;
+  description: string;
+};
+
 export class Historic extends Realm.Object<Historic> {
   _id!: string;
   user_id!: string;
@@ -27,9 +33,15 @@ export class Historic extends Realm.Object<Historic> {
     },
   }
 
-  static generate() {
+  static generate({ user_id, license_plate, description }: GenerateProps) {
     return {
-
+      _id: new Realm.BSON.UUID(),
+      user_id,
+      license_plate,
+      description,
+      status: 'departure',
+      created_at: new Date(),
+      updated_at: new Date(),
     };
   }
 }
