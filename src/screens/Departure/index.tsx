01ba9c373487b77cbdb76ec4ea251a2bb871292d
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-05-25 08:08:26 -0300

    Criando input para placa do veículo

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,11 +1,19 @@
 import { Header } from '../../components/Header';
+import { LicensePlateInput } from '../../components/LicensePlateInput';
 
-import { Container } from './styles';
+import { Container, Content } from './styles';
 
 export function Departure() {
   return (
     <Container>
       <Header title="Saída" />
+
+      <Content>
+        <LicensePlateInput
+          label="Placa do veículo"
+          placeholder="BRA1234"
+        />
+      </Content>
     </Container>
   );
 }
--- src/screens/Departure/styles.ts
+++ src/screens/Departure/styles.ts
@@ -7,3 +7,10 @@ export const Container = styled.View`
     background-color: ${theme.COLORS.GRAY_800};
   `};
 `;
+
+export const Content = styled.View`
+  flex: 1;
+  gap: 16px;
+  padding: 32px;
+  margin-top: 16px;
+`;
