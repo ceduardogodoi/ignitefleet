18b574d36629aff21e7d035e371012b7bd82667c
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-05-28 09:54:26 -0300

    Exibindo dados do banco

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,14 +3,28 @@ import { useNavigation } from '@react-navigation/native';
 import { HomeHeader } from '../../components/HomeHeader';
 import { CarStatus } from '../../components/CarStatus';
 
+import { useQuery } from '../../libs/realm';
+import { Historic } from '../../libs/realm/schemas/Historic';
+
 import { Container, Content } from './styles';
+import { useEffect } from 'react';
 
 export function Home() {
   const { navigate } = useNavigation();
 
+  const historic = useQuery(Historic);
+
   function handleRegisterMovement() {
     navigate('departure');
   }
+
+  function fetchVehicle() {
+    console.log(historic);
+  }
+
+  useEffect(() => {
+    fetchVehicle();
+  }, []);
 
   return (
     <Container>
