e523e710f672b03bf2e26b710e9292f4a60ae1e9
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-06-06 09:25:55 -0300

    Atualizando o ícone do status

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -10,6 +10,7 @@ import { HistoricCard, HistoricCardProps } from '../../components/HistoricCard';
 
 import { useQuery, useRealm } from '../../libs/realm';
 import { Historic } from '../../libs/realm/schemas/Historic';
+import { getLastSyncTimestamp, saveLastSyncTimestamp } from '../../libs/asyncStorage/asyncStorage';
 
 import { Container, Content, Label, Title } from './styles';
 
@@ -41,14 +42,17 @@ export function Home() {
     }
   }
 
-  function fetchHistoric() {
+  async function fetchHistoric() {
     try {
       const response = historic.filtered("status = 'arrival' SORT(created_at DESC)");
+
+      const lastSync = await getLastSyncTimestamp();
+
       const formattedHistoric = response.map(item => {
         return {
           id: item._id.toString(),
           licensePlate: item.license_plate,
-          isSync: false,
+          isSync: lastSync > item.updated_at.getTime(),
           created_at: dayjs(item.created_at).format('[Saída em] DD/MM/YYYY [às] HH:mm')
         };
       });
@@ -63,9 +67,12 @@ export function Home() {
     navigate('arrival', { id });
   }
 
-  function progressNotification(transferred: number, transferable: number) {
+  async function progressNotification(transferred: number, transferable: number) {
     const percentage = (transferred / transferable) * 100;
-    console.log('percentage::', `${percentage}%`);
+    if (percentage === 100) {
+      await saveLastSyncTimestamp();
+      await fetchHistoric();
+    }
   }
 
   useEffect(() => {
