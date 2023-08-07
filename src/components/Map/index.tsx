4bb5dde2d2159c9bb67bd86016a00b197d6fc56b
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-08-07 10:59:33 -0300

    Ajustando o mapa para exibir marcadores

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import MapView, {
   LatLng,
   MapViewProps,
@@ -13,10 +14,26 @@ type Props = MapViewProps & {
 };
 
 export function Map({ coordinates, ...rest }: Props) {
+  const mapRef = useRef<MapView>(null);
+
   const lastCoordinate = coordinates[coordinates.length - 1];
+
+  async function onMapLoaded() {
+    if (coordinates.length > 1) {
+      mapRef.current?.fitToSuppliedMarkers(['departure', 'arrival'], {
+        edgePadding: {
+          top: 50,
+          right: 50,
+          bottom: 50,
+          left: 50,
+        }
+      });
+    }
+  }
 
   return (
     <MapView
+      ref={mapRef}
       provider={PROVIDER_GOOGLE}
       style={{ width: '100%', height: 200 }}
       region={{
@@ -25,14 +42,15 @@ export function Map({ coordinates, ...rest }: Props) {
         latitudeDelta: 0.005,
         longitudeDelta: 0.005,
       }}
+      onMapLoaded={onMapLoaded}
       {...rest}
     >
-      <Marker coordinate={coordinates[0]}>
+      <Marker identifier="departure" coordinate={coordinates[0]}>
         <IconBox size="SMALL" icon={Car} />
       </Marker>
 
       {coordinates.length > 1 && (
-        <Marker coordinate={lastCoordinate}>
+        <Marker identifier="arrival" coordinate={lastCoordinate}>
           <IconBox size="SMALL" icon={FlagCheckered} />
         </Marker>
       )}
